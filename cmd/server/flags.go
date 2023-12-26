3a4b021a1bb7f5cf8f2ad698cabf3bd06ed69437
a.soltan <and.soltan@yandex.ru>
2023-12-26 21:15:22 +0300

    iter9: filestorage

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -9,15 +9,24 @@ import (
 )
 
 var flagAddr string
+var flagInterval int
+var flagPath string
+var flagRestore bool
 
 type Config struct {
-	Addr string `env:"ADDRESS"`
+	Addr     string `env:"ADDRESS"`
+	Interval int    `env:"STORE_INTERVAL"`
+	Path     string `env:"FILE_STORAGE_PATH"`
+	Restore  bool   `env:"RESTORE"`
 }
 
 func parseFlags() {
 	l := logger.Get()
 
 	flag.StringVar(&flagAddr, "a", "localhost:8080", "address and port metrics http server")
+	flag.IntVar(&flagInterval, "i", 300, "store metrics interval")
+	flag.StringVar(&flagPath, "f", "/tmp/metrics-db.json", "path to store metrics")
+	flag.BoolVar(&flagRestore, "r", true, "restore metrics from file")
 	flag.Parse()
 
 	var cfg Config
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/soltanat/metrics/internal/filestorage"
 	"github.com/soltanat/metrics/internal/handler"
 	"github.com/soltanat/metrics/internal/logger"
 	"github.com/soltanat/metrics/internal/storage"
@@ -12,10 +18,40 @@ func main() {
 	l := logger.Get()
 
 	s := storage.NewMemStorage()
-	h := handler.New(s)
 
-	err := handler.SetupRoutes(h).Start(flagAddr)
+	interval := time.Duration(flagInterval) * time.Second
+	fs, err := filestorage.New(s, interval, flagPath, flagRestore)
 	if err != nil {
 		l.Fatal().Err(err)
 	}
+	err = fs.Start()
+	if err != nil {
+		l.Fatal().Err(err)
+	}
+
+	h := handler.New(fs)
+
+	server := handler.SetupRoutes(h)
+
+	go func() {
+		err = server.Start(flagAddr)
+		if err != nil {
+			l.Error().Err(err)
+		}
+	}()
+
+	gracefulShutdown()
+	fs.Stop()
+	err = server.Close()
+
+	if err != nil {
+		l.Error().Err(err)
+	}
+}
+
+func gracefulShutdown() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(ch)
+	<-ch
 }
